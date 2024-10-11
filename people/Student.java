274aa848369ed474abec629e9cee2e484b0008e7
隔壁老王在弹琴 <jskr0168@sina.com>
2024-10-11 17:14:17 +0800

    首次添加Student.java!

---
+++ people/Student.java
@@ -0,0 +1,9 @@
+public class Student{
+
+    public static void main(String[] args) {
+
+        System.out.println("学生需要好好学习！");
+
+        System.out.println("Student are the important talants of a country!");
+    }
+}
